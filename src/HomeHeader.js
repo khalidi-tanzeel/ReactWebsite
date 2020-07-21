ae2ff581f658d7cf7f664a12b8bfd4574879c276
Tanzeel <67885561+khalidi-tanzeel@users.noreply.github.com>
2020-07-22 09:49:20 +1000

    Add files via upload

---
+++ src/HomeHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './Header.module.css';
+import { Link } from 'react-router-dom';
+
+const header = () => {
+    return(
+        <div className = { classes.header }>
+         <Link to = "/" className = { classes.logo }>HAPPY TOURS</Link>
+          <div className = { classes.headerright }>
+          <Link to = '/Home' className = { classes.active } >Home</Link>
+          <Link to = './contacts'>Contact</Link>
+          <Link to = '/about'>About Us</Link>
+           </div>
+        </div>
+    );
+}
+
+export default header
