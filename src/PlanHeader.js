32fafde95460d9ef5f30f3e8a752a3ddcb30c018
Tanzeel <67885561+khalidi-tanzeel@users.noreply.github.com>
2020-07-22 10:13:36 +1000

    Add files via upload

---
+++ src/PlanHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './Header.module.css';
+import { Link } from 'react-router-dom';
+
+const header = () => {
+    return(
+ <div className = { classes.header }>
+         <Link to = "/" className = { classes.logo }>HAPPY TOURS</Link>
+          <div className = { classes.headerright }>
+          <Link to = '/Home' >Home</Link>
+          <Link to = './contacts'>Contact</Link>
+          <Link to = '/about' >About Us</Link>
+          <Link to = '/trip' className = { classes.active }>Plan Trip</Link>
+           </div>
+        </div>
+    );
+}
+
+export default header
---
+++ src/trip.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PlanHeader from './PlanHeader';
+
+const trip = () => {
+    return(
+        <div>
+            <PlanHeader/>
+        </div>
+    );
+}
+
+export default trip
