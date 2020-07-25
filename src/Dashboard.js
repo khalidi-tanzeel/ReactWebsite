12baa4524e33f428e8980df6ef5bb4f3f64aec21
Tanzeel <67885561+khalidi-tanzeel@users.noreply.github.com>
2020-07-25 20:02:11 +1000

    Add files via upload

---
+++ src/Dashboard.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Card, CardContent, CardHeader } from '@material-ui/core';
+
+export default () => (
+    <Card>
+        <CardHeader title="Welcome Admin," />
+        <CardContent>Here's the complete data of users that booked the vacations with us</CardContent>
+    </Card>
+);
