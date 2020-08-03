6bece157eb9ac68953d2049cd4f76809e69bff1b
Tanzeel <67885561+khalidi-tanzeel@users.noreply.github.com>
2020-08-03 19:13:40 +1000

    Update deals.js

--- src/deals.js
+++ src/deals.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import classes from './deals.module.css';
+import { Link } from 'react-router-dom';
 
 const deals = () => {
     return (
         <div>
             <Header />
             <h2 style={{textAlign:"center", fontSize:"30px"}}>Discounted Prices</h2>
-            <p  style={{textAlign:"center",fontSize:"20px"}}>You can signup to enjoy the perks of these deals.</p>
+            <p  style={{textAlign:"center",fontSize:"20px"}}>You can signup to enjoy the perks of these deals or Wants to go <Link to ='/Register'>Back</Link>.</p>
             <div className={classes.columns}>
                 <ul className={classes.price}>
                     <li className={classes.header} style={{ backgroundColor: "#9CAF59" }}>Basic</li>
