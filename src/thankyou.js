31d9a8c1b714122f144ed562c7dead6f8b49f2b1
Tanzeel <67885561+khalidi-tanzeel@users.noreply.github.com>
2020-08-04 10:37:59 +1000

    Update thankyou.js

--- src/thankyou.js
+++ src/thankyou.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from './Header';
+import { Link } from 'react-router-dom';
+
 
 
 const thankyou = () => {
     return (
     <div>
         <Header />
+        <h4 style={{marginTop:100, marginBottom:300, textAlign:'center', marginLeft:20, marginRight:20}}>Hi User, Thanks for the booking with us, we will shortly send you the email regarding confirmation of your order</h4>
+        <Link to ="/">OK</Link>
 
-    </div>
-    );
+</div>    
+);
 }
 
 
