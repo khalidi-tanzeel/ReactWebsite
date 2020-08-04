d48583f5fc2fef4b72bedc9f050c9fd857cb4603
Tanzeel <67885561+khalidi-tanzeel@users.noreply.github.com>
2020-08-04 10:49:12 +1000

    Update thankyou.js

--- src/thankyou.js
+++ src/thankyou.js
@@ -8,8 +8,10 @@ const thankyou = () => {
     return (
     <div>
         <Header />
-        <h4 style={{marginTop:100, marginBottom:300, textAlign:'center', marginLeft:20, marginRight:20}}>Hi User, Thanks for the booking with us, we will shortly send you the email regarding confirmation of your order</h4>
-        <Link to ="/">OK</Link>
+        <h4 style={{marginTop:100, marginBottom:300, textAlign:'center', marginLeft:20, marginRight:20}}>Hi User, Thanks for the booking with us, we will shortly send you the email regarding confirmation of your order <br/>
+        <br/>
+        <Link to ="/">OK</Link></h4>
+       
 
 </div>    
 );
