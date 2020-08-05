56bad073314efea122a5b110ae100b732ec2bc8a
Tanzeel <67885561+khalidi-tanzeel@users.noreply.github.com>
2020-08-05 10:04:50 +1000

    Update Payment.js

--- src/Payment.js
+++ src/Payment.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React , {Component} from 'react';
 import Header from './Header';
 import classes from './Payment.module.css';
-const Payment = () => {
+class Payment extends Component {
+
+    handsleSubmit = (event) => {
+        event.preventDefault();
+        this.props.history.push('/ConfirmBooking');
+    }
+    render (){
     return (
         <div>
             <Header />
             <div className={classes.row}>
                 <div className={classes.col75}>
                     <div className={classes.container}>
-                        <form>
+                        <form onSubmit={this.handsleSubmit}>
 
                             <div className={classes.row}>
                                 <div className={classes.col25} >
 
                                     <h3>Billing Address</h3>
                                     <label for="fname" className={classes.div}> Full Name</label>
-                                    <input type="text" className={classes.div} id="fname" name="firstname" placeholder="Tanzeel Rahim" required />
+                                    <input type="text" className={classes.div} id="fname" name="firstname" placeholder="John M. Doe" required />
                                     <label for="email" className={classes.div}> Email</label>
-                                    <input type="text" className={classes.div} id="email" name="email" placeholder="tanzeel@example.com" required />
+                                    <input type="email" className={classes.div} id="email" name="email" placeholder="john@example.com" required />
                                     <label for="adr" className={classes.div} > Address</label>
                                     <input type="text" className={classes.div} id="adr" name="address" placeholder="542 W. 15th Street" required />
                                     <label for="city" className={classes.div}> City</label>
-                                    <input type="text" className={classes.div} id="city" name="city" placeholder="Australia" required />
+                                    <input type="text" className={classes.div} id="city" name="city" placeholder="New York" required />
 
                                     <div className={classes.row}>
                                         <div className={classes.col50}>
@@ -39,15 +45,15 @@ const Payment = () => {
                                 <div className={classes.col50}>
                                     <h3>Payment</h3>
                                     <label for="cname" className={classes.div}>Name on Card</label>
-                                    <input type="text" className={classes.div} id="cname" name="cardname" placeholder="Tanzeel Rahim" required />
+                                    <input type="text" className={classes.div} id="cname" name="cardname" placeholder="John More Doe" required />
                                     <label for="ccnum" className={classes.div}>Credit card number</label>
-                                    <input type="number" className={classes.div} id="ccnum" name="cardnumber" placeholder="1111222233334444" required />
+                                    <input type="number" className={classes.div} id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" required />
                                     <label for="expmonth" className={classes.div} >Exp Month</label>
                                     <input type="text" className={classes.div} id="expmonth" name="expmonth" placeholder="September" required />
                                     <div className={classes.row}>
                                         <div className={classes.col50}>
                                             <label for="expyear" className={classes.div} >Exp Date</label>
-                                            <input type="date" className={classes.div} id="expyear" name="expyear" placeholder="9-10-2018" required />
+                                            <input type="date" className={classes.div} id="expyear" name="expyear" placeholder="2018" required />
                                         </div>
                                         <div className={classes.col50}>
                                             <label for="cvv" className={classes.div} >CVV</label>
@@ -72,6 +78,7 @@ const Payment = () => {
         </div>
 
     );
+}
 }
 
 export default Payment
