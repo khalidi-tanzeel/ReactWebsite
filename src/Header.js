d7836498c380c0c4a0e574661eda7d5debabe09c
Tanzeel <67885561+khalidi-tanzeel@users.noreply.github.com>
2020-08-20 09:58:54 +1000

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -7,8 +7,8 @@ const header = () => {
         <div className = { classes.header }>
          <Link to = "/" className = { classes.logo }>HAPPY TOURS</Link>
           <div className = { classes.headerright }>
-          <Link to = '/Home'>Home</Link>
-          <Link to = './contacts'>Contact</Link>
+          <Link to = '/Home'>Places to go</Link>
+          <Link to = './contacts'>Contact Us</Link>
           <Link to = '/about'>About Us</Link>
           <Link to = '/trip'>Plan Trip</Link>
            </div>
